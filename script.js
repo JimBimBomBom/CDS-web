e372011a69dd2252d66d5560e61da846809d0bc4
JimBimBomBom <JimBimBomBom@users.noreply.github.com>
2024-08-09 14:16:45 +0000

    deploy: 1c807c16055db209ee2f7051107ce31996547faf

---
+++ script.js
@@ -0,0 +1,33 @@
+document.getElementById('searchBtn').addEventListener('click', function() {
+    const city1 = document.getElementById('city1').value;
+    const city2 = document.getElementById('city2').value;
+
+    console.log('Sending request to the server with:', { City1: city1, City2: city2 });
+
+    const username = process.env.AUTH_USERNAME;
+    const password = process.env.AUTH_PASSWORD;
+    const credential = btoa(`${username}:${password}`);
+
+    fetch('https://citydistanceservice-app-v6cgvtuw4a-uc.a.run.app/distance', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Connection': 'keep-alive',
+            'Authorization': `Basic ${credential}`
+        },
+        body: JSON.stringify({ City1: city1, City2: city2 })
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return response.json();
+    })
+    .then(data => {
+        document.getElementById('result').innerText = `Distance between '${city1}' and '${city2}' is: ${data}`;
+    })
+    .catch(error => {
+        console.error('Error:', error);
+        document.getElementById('result').innerText = 'An error occurred. Please try again later.';
+    });
+});
